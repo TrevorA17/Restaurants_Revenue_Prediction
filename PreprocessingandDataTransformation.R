6ccb5eba06b01a9cbc3cde64ca0f1438e20419da
Trevor Augustine <122263629+TrevorA17@users.noreply.github.com>
2024-05-30 13:36:25 +0300

    preprocessing

--- PreprocessingandDataTransformation.R
+++ PreprocessingandDataTransformation.R
@@ -21,3 +21,22 @@ print(missing_values_summary)
 # Total number of missing values in the dataset
 total_missing_values <- sum(is.na(restaurant_data))
 print(paste("Total number of missing values in the dataset:", total_missing_values))
+
+# Round off specified columns to 2 decimal places
+restaurant_data <- restaurant_data %>%
+  mutate(
+    Monthly_Revenue = round(Monthly_Revenue, 2),
+    Average_Customer_Spending = round(Average_Customer_Spending, 2),
+    Marketing_Spend = round(Marketing_Spend, 2),
+    Menu_Price = round(Menu_Price, 2)
+  )
+
+
+# Display the first few rows of the dataset to verify the rounding
+head(restaurant_data)
+
+# Display the structure of the dataset to verify the data type
+str(restaurant_data)
+
+# View the dataset in a separate viewer window
+View(restaurant_data)
